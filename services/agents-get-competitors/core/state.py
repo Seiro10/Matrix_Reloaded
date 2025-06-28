5bf00ceb25e79e416e63d556804cecc64fb64a55
Romain D <romaindelissen@protonmailcom>
2025-06-28 10:49:17 +0000

    [feat] Fix rebuilding duplicated images

--- services/agents-get-competitors/core/state.py
+++
@@ -1,7 +0,0 @@
-from typing import TypedDict, List, Dict, Any
-
-
-class State(TypedDict):
-    input_json: List[Dict[str, Any]]
-    urls_to_process: List[Dict[str, Any]]
-    processed: List[Dict[str, Any]]
